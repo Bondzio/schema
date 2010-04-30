851c7bcfb24a56ba39369b669bc71a425588e3a6
Martin Hylleberg <mhylle@users.noreply.github.com>
2010-04-30 15:57:35 +0000

    added userlist for administrators

--- src/com/scandihealth/olympicsc/user/controller/UserController.java
+++ src/com/scandihealth/olympicsc/user/controller/UserController.java
@@ -2,11 +2,16 @@ package com.scandihealth.olympicsc.user.controller;
 
 import com.scandihealth.olympicsc.data.DataManager;
 import com.scandihealth.olympicsc.user.User;
+import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.datamodel.DataModel;
+import org.jboss.seam.annotations.datamodel.DataModelSelection;
 
 import javax.faces.component.UIInput;
+import java.util.ArrayList;
+import java.util.List;
 
 @Name("userController")
 public class UserController {
@@ -16,8 +21,23 @@ public class UserController {
     private User user;
     private UIInput shirtSizeControl;
 
+
+    @DataModelSelection
+    private User selectedUser;
+
+    @DataModel
+    private List<User> userList;
+
     public UserController() {
     }
+
+    @Factory("userList")
+    public void findUsers() {
+        userList = new ArrayList<User>();
+        DataManager dataManager = new DataManager();
+        userList = dataManager.getUsers();
+    }
+
 
     private void validateShirtSizeControl() {
         if (shirtSizeControl != null) {
