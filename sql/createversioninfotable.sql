7d2a7f03ab36eecd75be87fbb422e42895742382
Martin Hylleberg <mhylle@users.noreply.github.com>
2010-05-04 19:14:33 +0000

    added a version info table sql

---
+++ sql/createversioninfotable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `olympicsc`.`versioninfo` (
+  `idversioninfo` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `versionnumber` VARCHAR(8) NOT NULL,
+  PRIMARY KEY (`idversioninfo`)
+)
+ENGINE = InnoDB;
