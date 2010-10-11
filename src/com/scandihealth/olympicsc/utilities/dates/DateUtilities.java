7a7f0591ef792558c47d4264b21fb2220e80c91a
Martin Hylleberg <mhylle@users.noreply.github.com>
2010-10-11 11:40:43 +0000

    added some utils

--- src/com/scandihealth/olympicsc/utilities/dates/DateUtilities.java
+++ src/com/scandihealth/olympicsc/utilities/dates/DateUtilities.java
@@ -3,6 +3,13 @@ package com.scandihealth.olympicsc.utilities.dates;
 import java.util.Date;
 
 public class DateUtilities {
+    public static boolean isBefore(Date date1, Date date2) {
+        return date1.before(date2);
+    }
+
+    public static boolean isAfter(Date date1, Date date2) {
+        return date1.after(date2);
+    }
 
     public static int dayDifference(Date date1, Date date2) {
         long date1Time = date1.getTime();
