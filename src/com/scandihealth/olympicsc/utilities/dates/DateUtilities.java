32d1cec8e1d506e5878436daf134b20dae7b5de2
Martin Hylleberg <mhylle@users.noreply.github.com>
2010-12-02 11:55:03 +0000

    iseaster method added

--- src/com/scandihealth/olympicsc/utilities/dates/DateUtilities.java
+++ src/com/scandihealth/olympicsc/utilities/dates/DateUtilities.java
@@ -25,4 +25,33 @@ public class DateUtilities {
     public boolean isDecember() {
         return new Date().getMonth() == 11;
     }
+
+    public static void main(String[] args) {
+         isEaster();
+    }
+    public static boolean isEaster() {
+        int year = new Date().getYear() + 1900;
+        int a = year % 19;
+        int b = year / 100;
+        int c = year % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1 ) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * l) / 451;
+        int easterMonth = (h + l - 7 * m + 114) / 31;
+        int easterDay = (h + l - 7 * m + 114) % 31 + 1;
+        System.out.println("easter this year was: " + easterDay + "/" + easterMonth + " " + year);
+
+        Date now = new Date();
+        Date easter = new Date();
+        easter.setMonth(easterMonth);
+        easter.setDate(easterDay);
+
+        return now.getMonth() == easter.getMonth() && now.getDate() == easter.getDate();
+    }
 }
