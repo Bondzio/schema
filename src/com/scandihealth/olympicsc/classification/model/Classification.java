4066e57962702feb163ed964012fc6224277a9b2
Martin Hylleberg <mhylle@users.noreply.github.com>
2011-02-04 19:14:28 +0000

    alpha 01 of classifications

--- src/com/scandihealth/olympicsc/classification/model/Classification.java
+++ src/com/scandihealth/olympicsc/classification/model/Classification.java
@@ -2,24 +2,46 @@ package com.scandihealth.olympicsc.classification.model;
 
 import org.jboss.seam.annotations.Name;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@javax.persistence.Table(name = "classification", catalog = "olympicsc")
 @Name("classification")
 public class Classification {
-    private Object name;
+    private String name;
     private ClassificationType type;
 
-    public void setName(Object name) {
+    private Set<ClassificationValue> classificationValues;
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public Object getName() {
+    public String getName() {
         return name;
     }
+
 
     public void setType(ClassificationType type) {
         this.type = type;
     }
 
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, optional = true)
+    @JoinColumn(name = "classificationTypeId")
     public ClassificationType getType() {
         return type;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinTable(name = "classification_has_value",
+            joinColumns = {@JoinColumn(name = "id_classification")},
+            inverseJoinColumns = {@JoinColumn(name = "id_classificationValue")})
+    public Set<ClassificationValue> getClassificationValues() {
+        return classificationValues;
+    }
+
+    public void setClassificationValues(Set<ClassificationValue> classificationValues) {
+        this.classificationValues = classificationValues;
     }
 }
